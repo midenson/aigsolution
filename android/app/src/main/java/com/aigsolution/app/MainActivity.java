8f1d67c699b715428d9f94353e7f22d14b8d3439
Mide <olatunjinaheem012@gmail.com>
2026-03-02 14:11:22 +0000

    built the app prototype

---
+++ android/app/src/main/java/com/aigsolution/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.aigsolution.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.aigsolution.app",
+  appName: "aigsolution",
+  webDir: "out",
+};
+
+export default config;
